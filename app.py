9c92a4e15155e2d235519ed1b703eb187eab9eca
Toby-A-eng <tobyroyanderson@gmail.com>
2025-11-20 10:18:47 +0000

    Re-added note deletion

--- app.py
+++ app.py
@@ -79,6 +79,16 @@ def notes():
     
     return render_template("notes.html", notes=notes)
 
+@app.route('/delete_note/<int:note_id>', methods=['GET', 'POST'])
+def delete_user(note_id):
+    if request.method == "POST":
+        with sqlite3.connect(DATABASE) as conn:
+            conn.execute('DELETE FROM notes WHERE id = ?', (note_id,))
+            conn.commit()
+        return redirect(url_for('notes'))
+    
+    return render_template('delete_note.html')
+
 if __name__ == '__main__':
     init_db()
     app.run(debug=True)
